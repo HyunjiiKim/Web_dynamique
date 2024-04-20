c215b21b88e1a0616043b9f1aa042c37c01bcc67
Hyunji <hjkim96727@gmail.com>
2024-04-20 23:36:53 +0200

    Create index files

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="includes/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div id="logo"></div>
+        <div id="banner"></div>
+        <nav>
+            <ol>
+                <li>Home</li>
+                <li>About</li>
+                <li>Article</li>
+                <li>Contact</li>
+            </ol>
+        </nav>
+    </header>
+    <section>
+        <div id="hero">
+            <input class="heroButton" type="button" value="Découvrir">
+        </div>
+        <div id="articleLinks">
+            <div class="articleAtHome"></div>
+            <div class="articleAtHome"></div>
+            <div class="articleAtHome"></div>
+        </div>
+    </section>
+    <footer>
+        <p>©2024 Hyunji KIM. All rights reserved. </p>
+    </footer>
+</body>
+</html>
